7be675a46aff4f223308b62072136a85328eb4d7
Gabriele Mirando <gmirando@macbook.local>
2024-10-26 14:08:40 +0200

    pbb: skip learning tests

--- project-be-books/tests/services/test_gutendex_api.py
+++ project-be-books/tests/services/test_gutendex_api.py
@@ -3,8 +3,12 @@ import requests
 
 
 class GutendexApiTestCase(unittest.TestCase):
+    @unittest.skip
     def test_search_book(self):
         response = requests.get("http://gutendex.com/books?search=Frankenstein")
+
+        self.assertEqual(response.status_code, 200)
+
         content = response.json()
 
         self.assertTrue("results" in content)
@@ -21,8 +25,12 @@ class GutendexApiTestCase(unittest.TestCase):
         first_author = first_result["authors"][0]
         self.assertTrue("name" in first_author)
 
+    @unittest.skip
     def test_get_book_detail(self):
         response = requests.get("http://gutendex.com/books/84")
+
+        self.assertEqual(response.status_code, 200)
+
         content = response.json()
 
         self.assertTrue("id" in content)
@@ -36,3 +44,16 @@ class GutendexApiTestCase(unittest.TestCase):
 
         first_author = content["authors"][0]
         self.assertTrue("name" in first_author)
+
+    @unittest.skip
+    def test_get_missing_book_detail(self):
+        response = requests.get("http://gutendex.com/books/875329578932759823")
+
+        self.assertEqual(response.status_code, 404)
+
+        content = response.json()
+
+        self.assertFalse("id" in content)
+        self.assertFalse("title" in content)
+        self.assertFalse("authors" in content)
+        self.assertFalse("subjects" in content)
